7256b1891d6dc2d414c139159e47cedf513bc9d9
Gino <gino.chasles@gmail.com>
2023-04-10 21:48:32 +0200

    docker compose file, and test for note service and begin controller test

--- notes/src/test/java/com/example/notes/controller/NoteControllerTest.java
+++ notes/src/test/java/com/example/notes/controller/NoteControllerTest.java
@@ -1,4 +1,142 @@
 package com.example.notes.controller;
 
+import com.example.notes.NotesApplication;
+import com.example.notes.model.Note;
+import com.example.notes.service.NoteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+@SpringBootTest(classes = NotesApplication.class)
+@AutoConfigureMockMvc
 public class NoteControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Mock
+    NoteService noteService;
+    @InjectMocks
+    NoteController noteController;
+    @Autowired
+    ObjectMapper mapper;
+
+    private Note note1;
+    private Note note2;
+
+    private List<Note> noteList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        note1 = new Note("randomKey", 1L, "Note 1 test");
+        note2 = new Note("randomKey2", 1L, "Note 2 test");
+
+        noteList.add(note1);
+        noteList.add(note2);
+    }
+
+
+    @Test
+    public void findByIdTest_NotFound() throws Exception {
+        mockMvc.perform(get("/patHistory/1"))
+                .andExpect(status().isNotFound());
+    }
+
+   /* @Test
+    public void findByIdTest() throws Exception {
+
+        Mockito.when(noteService.findById(anyString())).thenReturn(Optional.ofNullable(note1));
+        mockMvc.perform(get("/patHistory/{id}", "randomKey"))
+                .andExpect(status().isOk());
+    }*/
+
+    /*@Test
+    public void findAllTest() throws Exception {
+        mockMvc.perform(get("/patHistory/"))
+                .andExpect(status().isOk());
+    }*/
+
+    @Test
+    public void findAllByIdTest_NotFound() throws Exception {
+        mockMvc.perform(get("/patHistory/10000/all"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void findAllByIdTest() throws Exception {
+        mockMvc.perform(get("/patHistory/1/all"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void addNoteTest() throws Exception {
+
+        String json = mapper.writeValueAsString(note1);
+        mockMvc.perform(post("/patHistory/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk());
+    }
+
+   /* @Test
+    public void updateNoteTest() throws Exception {
+        Note updateNote = note1;
+        updateNote.setNote("Updating note 1");
+        String json = mapper.writeValueAsString(updateNote);
+
+        //TODO mock update
+
+        mockMvc.perform(put("/patHistory/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk());
+    }*/
+
+    @Test
+    public void updateNoteTest_NotFound() throws Exception {
+        Note updateNote = note1;
+        updateNote.setNote("Updating note 1");
+        String json = mapper.writeValueAsString(updateNote);
+
+        mockMvc.perform(put("/patHistory/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteTest_NotFound() throws Exception {
+        mockMvc.perform(delete("/patHistory/{id}", 1))
+                .andExpect(status().isNotFound());
+    }
+
+   /* @Test
+    public void deleteTest() throws Exception {
+        mockMvc.perform(delete("/patHistory/{id}", 1))
+                .andExpect(status().isOk());
+    }*/
+
+    @Test
+    public void deleteAllTest() throws Exception {
+        mockMvc.perform(delete("/patHistory/"))
+                .andExpect(status().isOk());
+    }
 }
--- notes/src/test/java/com/example/notes/service/NoteServiceTest.java
+++ notes/src/test/java/com/example/notes/service/NoteServiceTest.java
@@ -1,4 +1,104 @@
 package com.example.notes.service;
 
+import com.example.notes.model.Note;
+import com.example.notes.repository.NoteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class NoteServiceTest {
+    @InjectMocks
+    private NoteServiceImpl noteService;
+
+    @Mock
+    private NoteRepository noteRepository;
+
+    private Note note1;
+    private Note note2;
+
+    private List<Note> noteList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        note1 = new Note("randomKey", 1L, "Note 1 test");
+        note2 = new Note("randomKey2", 1L, "Note 2 test");
+
+        noteList.add(note1);
+        noteList.add(note2);
+    }
+
+
+    @Test
+    public void findByIdTest() {
+        when(noteRepository.findByKey(anyString())).thenReturn(Optional.of(note1));
+        Optional<Note> optionalNote = noteService.findById(note1.getKey());
+        if (optionalNote.isPresent()) {
+            Note note = optionalNote.get();
+            assertThat(note.getNote()).isEqualTo(note1.getNote());
+        }
+    }
+
+    @Test
+    public void findAllTest() {
+        when(noteRepository.findAll()).thenReturn(noteList);
+        List<Note> result = noteService.findAll();
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getKey()).isEqualTo(note1.getKey());
+        assertThat(result.get(1).getKey()).isEqualTo(note2.getKey());
+    }
+
+    @Test
+    public void findByPatIdTest() {
+        when(noteRepository.findAllByPatId(anyLong())).thenReturn(noteList);
+        List<Note> result = noteService.findByPatId(1L);
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getKey()).isEqualTo(note1.getKey());
+        assertThat(result.get(1).getKey()).isEqualTo(note2.getKey());
+    }
+
+    @Test
+    public void insertTest() {
+        when(noteRepository.insert(any(Note.class))).thenReturn(note1);
+        Note result = noteService.insert(note1);
+        assertThat(result.getNote()).isEqualTo(note1.getNote());
+    }
+
+    @Test
+    public void updateTest() {
+        when(noteRepository.findByKey(anyString())).thenReturn(Optional.of(note1));
+        Note noteToUpdate = note1;
+        noteToUpdate.setNote("Updating note 1");
+        when(noteRepository.save(noteToUpdate)).thenReturn(noteToUpdate);
+
+        Note result = noteService.update(note1.getKey(), noteToUpdate);
+        if (result != null) {
+            assertThat(result.getNote()).isEqualTo(noteToUpdate.getNote());
+        }
+    }
+
+    @Test
+    public void updateTest_NoteNotPresent() {
+        when(noteRepository.findByKey(anyString())).thenReturn(Optional.empty());
+        Note noteToUpdate = note1;
+        noteToUpdate.setNote("Updating note 1");
+        Note result = noteService.update(note1.getKey(), noteToUpdate);
+        assertThat(result).isNull();
+
+    }
+
+
 }
